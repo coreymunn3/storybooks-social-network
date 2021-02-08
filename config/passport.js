ab9c14747fcaba9236a05ce643d18ac63137d876
Michael Corey Munn <coreymunn3@gmail.com>
2021-02-08 00:04:02 -0500

    Fixed redirect problem and successful authentication now redirects to react app dashbaord instead of server route /dashbaord.

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ module.exports = function () {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:5000/auth/google/callback',
+        callbackURL: 'http://localhost:3000/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         // console.log(profile);
